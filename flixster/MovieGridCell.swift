2260a5241058dd5853e72340cf985dd6e693317f
SusiesuS <76606272+SusiesuS@users.noreply.github.com>
2021-01-18 20:15:45 -0800

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Qingshuang Su on 1/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
